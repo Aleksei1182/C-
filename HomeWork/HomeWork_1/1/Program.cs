5c4c4992b46b6c26ac00705f26b4098e789e7823
Aleksei <Zaharov.A82@mail.ru>
2022-12-04 21:29:23 +0500

    решена первая задача

---
+++ HomeWork/HomeWork_1/1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+Console.Write("max = ");
+Console.WriteLine(num1);
+Console.Write("min = ");
+Console.WriteLine(num2);
+}
+else 
+{
+Console.Write("max = ");
+Console.WriteLine(num2);
+Console.Write("min = ");
+Console.WriteLine(num1);
+}
