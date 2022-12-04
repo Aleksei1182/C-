37ba85107d51101e9ceb412e7e7c1e39b2fd0a9d
Aleksei <Zaharov.A82@mail.ru>
2022-12-04 22:53:49 +0500

    решена четвёртая задача

---
+++ HomeWork/HomeWork_1/4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while(i <= num)
+{
+    if(i % 2 == 0)
+    {
+    Console.WriteLine(i);
+    i = i + 1;
+    }
+    else
+    {
+    i = i + 1;
+    }
+}
