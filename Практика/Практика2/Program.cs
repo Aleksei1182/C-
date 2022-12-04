75572e7e99e44845b7020648b39c6879e86659af
Aleksei <Zaharov.A82@mail.ru>
2022-12-04 18:37:29 +0500

    решил первые две практические задачи

--- Практика/Практика2/Program.cs
+++ Практика/Практика2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number2 * number2 == number1)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+Console.WriteLine("Нет");
+}
