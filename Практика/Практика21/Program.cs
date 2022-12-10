d9c6c1fd9728a28583b4f510ce658b04adbb0590
Aleksei <Zaharov.A82@mail.ru>
2022-12-10 16:29:44 +0500

    решил задачи по практике №2

---
+++ Практика/Практика21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа выводит случайное число из диапазона от 10 до 99 и определяет какая цифра больше в цисле.
+
+int N = new Random().Next(10, 99);
+Console.WriteLine(N);
+
+int firstDigit = N / 10;
+int secondDigit = N % 10;
+
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
---
+++ Практика/Практика22/Program.cs
@@ -0,0 +1,29 @@
+﻿//принимает на входе два числа и пределяет является ли второе число кратны первому. Если не кратное то выводит остаток от деления
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if (a > b)
+{
+    if(a % b == 0)
+    {
+        Console.WriteLine("Кратное");
+    }
+    else
+    {
+        int result = a % b;
+        Console.WriteLine("Не кратное. Остаток равен " + result);
+    }
+}
+else
+ if(b % a == 0)
+    {
+        Console.WriteLine("Кратное");
+    }
+    else
+    {
+        int result = b % a;
+        Console.WriteLine("Не кратное. Остаток равен " + result);
+    }
