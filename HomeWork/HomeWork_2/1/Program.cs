e194052e6c743786992516a13133d7b877ec12f7
Aleksei <Zaharov.A82@mail.ru>
2022-12-10 18:12:09 +0500

    решены домашнии задачи от 2 практики

---
+++ HomeWork/HomeWork_2/1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number / 10;
+int number3 = number2 % 10;
+Console.WriteLine(number3);
---
+++ HomeWork/HomeWork_2/2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа, которая выводит третью цифру заданного числа (от 1 до 65636) или сообщает, что третьей цифры нет (решение должно быть математическим).
+
+//Console.WriteLine("Введите число от 1 до 65637: ");
+//int N = Convert.ToInt32(Console.ReadLine());
+
+int N = new Random().Next(1, 65637);
+Console.WriteLine(N);
+
+if (N / 10000 > 0)
+{
+    int preliminary = N / 100;
+    int result = preliminary % 10;
+    Console.WriteLine("третья цифра в числе: " + result);
+}
+else if (N / 1000 > 0)
+{
+    int preliminary = N / 10;
+    int result = preliminary % 10;
+    Console.WriteLine("третья цифра в числе: " + result);
+}
+else if (N / 100 > 0)
+{
+    int result = N % 10;
+    Console.WriteLine("третья цифра в числе: " + result);
+}
+else
+{
+    Console.WriteLine("третьей цифры в числе нет ");
+}
