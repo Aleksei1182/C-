6b8109dfd4301e41104ca7e6f0cb6d86df7381e5
Aleksei <Zaharov.A82@mail.ru>
2022-12-11 17:17:03 +0500

    добавил вариант решения для объединения двух условий в одно

--- HomeWork/HomeWork_2/3/Program.cs
+++ HomeWork/HomeWork_2/3/Program.cs
@@ -2,7 +2,7 @@
 
 Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-
+/*
 if (N % 7 == 0)
 {
     if (N % 23 == 0)
@@ -13,6 +13,17 @@ if (N % 7 == 0)
     {
         Console.WriteLine("Введённое число делится только на 7");
     }
+}
+else
+{
+    Console.WriteLine("Введённое НЕ делится на 7 и на 23");
+}*/
+
+//Далее вариант когда объединяется два условия в одно
+
+if (N % 7 == 0 && N % 23 == 0)
+{
+    Console.WriteLine("Введённое число делится на 7 и на 23");   
 }
 else
 {
