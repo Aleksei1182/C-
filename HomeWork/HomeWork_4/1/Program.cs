c9398a9b1023e75cc7c9f1aad8ee88e657b5a8bb
Aleksei <Zaharov.A82@mail.ru>
2022-12-16 23:35:29 +0500

    решил 1 и 2 задачу в 4 домашке

---
+++ HomeWork/HomeWork_4/1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на входе два числа (A и B) и возводит число A в натуральную степень B. 
+// Не использовать функцию Math.Pow
+
+Console.WriteLine("Введите число, которое вы хотите возвести в спепень: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число, которое будет определять степень возведения для первого числа: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int ResultOfExponentiation = 1;
+
+for(int i = 0; i < num2; i++)
+{
+    ResultOfExponentiation = ResultOfExponentiation * num1;
+}
+Console.WriteLine(ResultOfExponentiation);
---
+++ HomeWork/HomeWork_4/2/Program.cs
@@ -0,0 +1,27 @@
+﻿// программа, которая принимает на вход число и выдаёт сумму цифр в числе, которые являются делителями этого числа.
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int NumberDigits1 = N;
+int count = 0; //Кол-во цифр в числе
+    while(NumberDigits1 > 0)
+    {
+        NumberDigits1 = NumberDigits1 / 10;
+        count++;
+    }
+
+int[] digitsOfNumber = new int[count];
+int NumberDigits2 = N;
+for (int i = 0; i < count; i++)
+{
+    digitsOfNumber[i] = NumberDigits2 % 10;
+    NumberDigits2 = NumberDigits2 / 10;
+}
+
+int SumOfDivisors = 0;
+for (int i = 0; i < count; i++)
+{
+    if (N % digitsOfNumber[i] == 0) SumOfDivisors = SumOfDivisors + digitsOfNumber[i];
+}
+Console.WriteLine(SumOfDivisors);
