2bf215ffdf8195ac22660443b6c47da68823f338
Aleksei <Zaharov.A82@mail.ru>
2022-12-28 21:16:46 +0500

    разобрался в лекции 4

---
+++ ExampleLecture№4/2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 
+
+int[,] matrix= new int[3, 4];
+
+//for (int i = 0; i < 3; i++)
+// аналог верхней записи показан ниже через "matrix.GetLength(0)"
+for (int i = 0; i < matrix.GetLength(0); i++) // "0" говорит нам что речь идёт о строках
+
+{
+    //for (int j = 0; j < 4; j++)
+    // аналог верхней записи показан ниже через "matrix.GetLength(1)"
+    for (int j = 0; j < matrix.GetLength(1); j++) // "1" говорит нам что речь идёт о столбцах
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();
+}
+
---
+++ ExampleLecture№4/5/Program.cs
@@ -0,0 +1,13 @@
+﻿//
+
+double Factorial(int n) // метод на примере вывода факториала нагалядно нам демонстрирует что такое Рекурсия (Функция циклится)
+{
+    if(n==1) return 1;
+    else return n * Factorial(n-1);
+}
+
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
